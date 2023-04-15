e7b542ed99a5a19be32a250b0bf99b2771babbeb
Carlos Vinícius <carlos.martins@cwi.com.br>
2023-04-14 21:39:47 -0300

    alterações no header

--- src/ui/components/Header/header.component.jsx
+++ src/ui/components/Header/header.component.jsx
@@ -12,7 +12,7 @@ export function Header() {
             <MenuMobile menuIsVisible={menuIsVisible} setMenuIsVisible={setMenuIsVisible}/>
             <header className="header">
                 <nav className="header__nav">
-                    <h1>Vinicarlosss</h1>
+                    <h1 className="header__logo"><span className="header__logo--span">C</span>arlos Vinícius</h1>
                     <ul className="header__ul">
                         <li className="header__li"><a className="header__li--link" href="/">Início</a></li>
                         <li className="header__li"><a className="header__li--link" href="/">Projetos</a></li>
