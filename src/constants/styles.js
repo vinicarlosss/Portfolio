5cc52ef3fc545b325a8d719f3275cfdaac5a69bd
Carlos Vinícius <carlos.martins@cwi.com.br>
2023-04-16 13:33:07 -0300

    refatorando código do menu mobile

---
+++ src/constants/styles.js
@@ -0,0 +1,8 @@
+export const WHENMENUISVISIBLEFILTER = {
+    backdropFilter: 'blur(3px)'
+}
+
+export const WHENMENUISVISIBLE = {
+    opacity: 1,
+    display:"flex",
+}
--- src/ui/components/MenuMobile/menuMobile.component.jsx
+++ src/ui/components/MenuMobile/menuMobile.component.jsx
@@ -1,6 +1,7 @@
 import "./menuMobile.style.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons'
+import {WHENMENUISVISIBLE, WHENMENUISVISIBLEFILTER} from "../../../constants/styles"
 
 export function MenuMobile({ menuIsVisible, setMenuIsVisible }) {
 
@@ -8,20 +9,15 @@ export function MenuMobile({ menuIsVisible, setMenuIsVisible }) {
         setMenuIsVisible(!menuIsVisible)
     }
 
-    const whenMenuIsVisible = {
-        opacity: 1,
-        display:"flex",
-    }
-
-    const whenMenuIsVisibleFilter = {
-        backdropFilter: 'blur(3px)'
+    const handlemenuisVisible = (css) =>{
+        if(menuIsVisible) return css
     }
 
     return (
         <>
-            <nav className="menuMobile__nav" style={menuIsVisible ? whenMenuIsVisibleFilter : null}>
+            <nav className="menuMobile__nav" style={handlemenuisVisible(WHENMENUISVISIBLEFILTER)}>
                 <FontAwesomeIcon className="menuMobile__hamburguer-menu--icon" onClick={toggleMode} icon={menuIsVisible ? faTimes : faBars} />
-                <ul className="menuMobile__ul" style={menuIsVisible ? whenMenuIsVisible:null}>
+                <ul className="menuMobile__ul" style={handlemenuisVisible(WHENMENUISVISIBLE)}>
                     <li className="menuMobile__li"><a onClick={toggleMode} className="menuMobile__li--link" href="/">Início</a></li>
                     <li className="menuMobile__li"><a onClick={toggleMode} className="menuMobile__li--link" href="/">Projetos</a></li>
                     <li className="menuMobile__li"><a onClick={toggleMode} className="menuMobile__li--link" href="/">Sobre mim</a></li>
