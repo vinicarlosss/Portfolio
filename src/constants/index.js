01e27ba2ff5262da1d6d682d1e9998d20915c78d
Carlos Vinícius <carlos.martins@cwi.com.br>
2023-09-20 08:10:32 -0300

    alterando email

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const INSTAGRAM_URL = "https://www.instagram.com/vinicarlosss/"
 export const LINKEDLN_URL = "https://www.linkedin.com/in/vinicarlosss/"
 export const GITHUB_URL = "https://github.com/vinicarlosss"
-export const EMAIL_IDENTIFIER = "carlos.martins@cwi.com.br"
+export const EMAIL_IDENTIFIER = "carlos.viniciusufrpe@gmail.com"
